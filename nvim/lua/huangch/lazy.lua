c8966abcd915bff79f1cd48e07f3c2372cd0d94f
huangchanghu <huangchanghu@shopline.com>
2024-08-26 20:36:10 +0800

    feat: plugins

--- nvim/lua/huangch/lazy.lua
+++ nvim/lua/huangch/lazy.lua
@@ -11,12 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("huangch.plugins", {
+require("lazy").setup({ { import = "huangch.plugins" }, { import = "huangch.plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,
   },
   change_detection = {
-    notify = true, -- notification for filte changed
+    notify = false, -- notification for filte changed
   },
 })
